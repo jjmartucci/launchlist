321244fe5604a9428f87aafb2eed600953be9f37
Joe Martucci <joe.martucci@gmail.com>
2018-03-08 22:37:58 -0500

    Removing all files now ignored.

--- launchlist-cli/src/app.js
+++ launchlist-cli/src/app.js
@@ -1,1 +1,26 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+import "whatwg-fetch";
+
 console.log("hello world");
+
+class App extends React.Component {
+  componentDidMount() {
+    fetch("/list")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        console.log("parsed json", json);
+      })
+      .catch(function(ex) {
+        console.log("parsing failed", ex);
+      });
+  }
+  render() {
+    return <h1>It's an app!</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
